61401db8177d517f8640971badd7e518e9957887
AndrewTaker <n0thingg@yandex.ru>
2025-02-03 14:43:54 +0300

    fix: further binding fix

--- imeicheck/api_calls.py
+++ imeicheck/api_calls.py
@@ -15,14 +15,10 @@ class Properties(BaseModel):
     imei2: str
     serial: str
     estimate_pruchase_date: int = Field(alias="estPurchaseDate")
-    gsm_blacklisted: bool = Field(alias="gsmBlacklisted")
     sim_lock: bool = Field(alias="simLock")
     replaced: bool
     apple_or_region: str = Field(alias="apple/region")
-    apple_or_model_name: str = Field(alias="apple/modelName")
     loaner: bool
-    lost_mode: str = Field(alias="lostMode")
-    usa_block_status: str = Field(alias="usaBlockStatus")
 
     model_config = {"populate_by_name": True}
 
@@ -42,14 +38,14 @@ class ImeiCheckResponse(BaseModel, extra="allow"):
     amount: str
     device_id: str = Field(alias="deviceId")
     processed_at: int = Field(alias="processedAt")
-    properties: Properties 
+    properties: Properties
 
     model_config = {"populate_by_name": True}
 
 class ImeiService:
     def __init__(self, base_url: str | None, token: str | None) -> None:
         if not base_url or not token:
-            raise ValueError("not base url or token")
+            raise ValueError("either base_url or token missing")
 
         self.base_url = base_url
         self.token = token
@@ -59,7 +55,7 @@ class ImeiService:
         }
 
 
-    def check_device(self, device_id: str) -> None:
+    def check_device(self, device_id: str) -> ImeiCheckResponse:
         url = f"{self.base_url}/v1/checks"
         payload = ImeiCheckRequest.model_construct(device_id=device_id, service_id=12)
 
@@ -68,9 +64,5 @@ class ImeiService:
             headers=self.headers,
             data=payload.model_dump_json(by_alias=True)
         )
-        r = ImeiCheckResponse.model_validate(response.json()[0])
-        print(r.model_dump_json())
 
-
-    def __repr__(self) -> str:
-        return f"base_url = {self.base_url}, token = {self.token}"
+        return ImeiCheckResponse.model_validate(response.json()[0])
