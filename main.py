3bf887bdd289ee17389ee667eda6e77790062dea
AndrewTaker <n0thingg@yandex.ru>
2025-02-03 19:26:47 +0300

    add: bot init + imei service context for handler

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 from imeicheck.api_calls import ImeiService
+from bot.handlers import start
+from telegram.ext import ApplicationBuilder, CommandHandler
 
-IMEI_TOKEN: str | None = os.getenv("IMEI_TOKEN")
-TELEGRAM_BOT_TOKEN: str | None = os.getenv("TELEGRAM_BOT_TOKEN")
+# print(imei_service.check_device("869908051763278").model_dump_json(by_alias=True))
 
-imei_service = ImeiService("https://api.imeicheck.net", IMEI_TOKEN)
-print(imei_service)
+if __name__ == "__main__":
+    IMEI_TOKEN: str | None = os.getenv("IMEI_TOKEN")
+    TELEGRAM_BOT_TOKEN: str | None = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    imei_service = ImeiService("https://api.imeicheck.net", IMEI_TOKEN)
+
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    application.bot_data["imei"] = imei_service
+
+    start_handler = CommandHandler('start', start)
+
+    application.add_handler(start_handler)
+    application.run_polling()
