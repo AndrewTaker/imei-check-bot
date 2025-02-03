2ce2869ed77e90310830504eea907f0767fd6bad
AndrewTaker <n0thingg@yandex.ru>
2025-02-03 14:36:52 +0300

    fix: model json binding

--- imeicheck/api_calls.py
+++ imeicheck/api_calls.py
@@ -1,43 +1,50 @@
 from pydantic import BaseModel, Field
+import requests
 
 class Service(BaseModel):
     id: int
     title: str
 
+    model_config = {"populate_by_name": True}
+
 class Properties(BaseModel):
     device_name: str = Field(alias="deviceName")
     image: str
     imei: str
+    meid: str
+    imei2: str
+    serial: str
     estimate_pruchase_date: int = Field(alias="estPurchaseDate")
+    gsm_blacklisted: bool = Field(alias="gsmBlacklisted")
     sim_lock: bool = Field(alias="simLock")
-    warranty_status: str = Field(alias="warrantyStatus")
-    repair_coverage: str = Field(alias="repairCoverage")
-    technical_support: str = Field(alias="technicalSupport")
-    model_description: str = Field(alias="modelDesc")
-    demo_unit: bool = Field(alias="demoUnit")
-    refurbished: bool
-    purchase_country: str = Field(alias="purchaseCountry")
+    replaced: bool
     apple_or_region: str = Field(alias="apple/region")
-    fmi_on: bool = Field(alias="fmi_on")
+    apple_or_model_name: str = Field(alias="apple/modelName")
+    loaner: bool
     lost_mode: str = Field(alias="lostMode")
     usa_block_status: str = Field(alias="usaBlockStatus")
-    network: str
 
+    model_config = {"populate_by_name": True}
 
 class ImeiCheckRequest(BaseModel):
     device_id: str = Field(alias="deviceId")
     service_id: int = Field(alias="serviceId")
 
-class ImeiCheckResponse(BaseModel):
+    model_config = {"populate_by_name": True}
+
+class ImeiCheckResponse(BaseModel, extra="allow"):
+    warning: str = Field(alias="!!! WARNING !!!")
     id: str
-    _type: str = Field(alias="type")
+    type: str = Field(alias="type")
     status: str
-    order_id: str = Field(alias="orderId")
+    order_id: str | None = Field(alias="orderId", default=None)
     service: Service
     amount: str
     device_id: str = Field(alias="deviceId")
     processed_at: int = Field(alias="processedAt")
-    properties: Properties
+    properties: Properties 
+
+    model_config = {"populate_by_name": True}
 
 class ImeiService:
     def __init__(self, base_url: str | None, token: str | None) -> None:
@@ -46,9 +53,24 @@ class ImeiService:
 
         self.base_url = base_url
         self.token = token
-        self.headers = {"Authorization": f"Bearer {token}"}
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-type": "application/json",
+        }
 
 
-    def ch
+    def check_device(self, device_id: str) -> None:
+        url = f"{self.base_url}/v1/checks"
+        payload = ImeiCheckRequest.model_construct(device_id=device_id, service_id=12)
+
+        response = requests.get(
+            url,
+            headers=self.headers,
+            data=payload.model_dump_json(by_alias=True)
+        )
+        r = ImeiCheckResponse.model_validate(response.json()[0])
+        print(r.model_dump_json())
+
+
     def __repr__(self) -> str:
         return f"base_url = {self.base_url}, token = {self.token}"
